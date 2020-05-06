8dedfc4ed89162f86bb5e4651f375556a1e78832
zinkyfmt <minhtinhph2302@gmail.com>
2020-05-06 14:44:53 +0700

    ff

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 //Add javascript code here
+// Get the header
+var header = document.getElementById("header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+window.onscroll = function() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+};
+setInterval(function() {
+    console.log('slider change');
+    $('.carousel-control.right').trigger('click');
+}, 4000);
